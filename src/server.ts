9c86b8b4be1f4c313dac98bee380411565c8c12d
leon morival <leon.morival@gmail.com>
2025-03-18 12:11:29 +0100

    docker server

--- src/server.ts
+++ src/server.ts
@@ -116,4 +116,6 @@ app.post("/handle-selection", async (req: Request, res: Response) => {
 });
 
 const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
